2a88bcc7687c206b354ebe7b0e311d36023b0c22
Sajid <osmansazid13@gmail.com>
2021-07-18 20:03:56 +0600

    Setting up Hilt

--- app/src/main/java/com/example/news_24_7/MainActivity.kt
+++ app/src/main/java/com/example/news_24_7/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.news_24_7
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/news_24_7/ui/NewsListFragment.kt
+++ app/src/main/java/com/example/news_24_7/ui/NewsListFragment.kt
@@ -2,6 +2,8 @@ package com.example.news_24_7.ui
 
 import androidx.fragment.app.Fragment
 import com.example.news_24_7.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class NewsListFragment: Fragment(R.layout.fragment_news_list) {
 }
