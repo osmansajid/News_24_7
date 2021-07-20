f139dfbf2fc408e2bd53599ad7a7b123b3063bc9
Sajid <osmansazid13@gmail.com>
2021-07-20 21:08:20 +0600

    Refactoring MainActivity.kt

--- app/src/main/java/com/example/news_24_7/ui/MainActivity.kt
+++ app/src/main/java/com/example/news_24_7/ui/MainActivity.kt
@@ -2,9 +2,7 @@ package com.example.news_24_7.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.core.app.ActivityCompat
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
@@ -14,8 +12,6 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.news_24_7.R
 import com.example.news_24_7.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -26,42 +22,29 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragment_main_container) as NavHostFragment
         navController = navHostFragment.findNavController()
 
-
-        //this doesn't work for some reason
-        /*binding.apply {
-           bottomBar.setupWithNavController(navController)
-        }*/
-
-        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
-        val toolbar = findViewById<Toolbar>(R.id.toolbar_)
-
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.newsListFragment,
-                R.id.sportsNewsListFragment,
-                R.id.entertainmentNewsListFragment,
-                R.id.searchNewsListFragment
-            ),
-            drawerLayout
+        binding.apply {
+            bottomBar.setupWithNavController(navController)
+            appBarConfiguration = AppBarConfiguration(
+                setOf(
+                    R.id.newsListFragment,
+                    R.id.sportsNewsListFragment,
+                    R.id.entertainmentNewsListFragment,
+                    R.id.searchNewsListFragment
+                ),
+                drawerLayout
             )
-
-
-        setSupportActionBar(toolbar)
+            setSupportActionBar(toolbar)
+            navDrawer.setupWithNavController(navController)
+        }
 
         setupActionBarWithNavController(navController, appBarConfiguration)
-
-        val bottomBar = findViewById<BottomNavigationView>(R.id.bottom_bar)
-        bottomBar.setupWithNavController(navController)
-
-        val navDrawer = findViewById<NavigationView>(R.id.nav_drawer)
-        navDrawer.setupWithNavController(navController)
-
     }
 
     override fun onBackPressed() {
